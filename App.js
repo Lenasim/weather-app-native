19fcee4523278750d81db98b031b03fd48b70677
LenaSim <radical.rin@gmail.com>
2020-08-06 15:47:54 +0200

    Call weather data via axios

--- App.js
+++ App.js
@@ -1,31 +1,45 @@
 import React from 'react';
-import { StyleSheet, View, Alert } from 'react-native';
+import { StyleSheet, View, Alert, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import * as Location from 'expo-location';
+import axios from 'axios';
 import Loading from './Loading';
 
+const API_KEY = "821f07c1a9e2948b18b4269a36323df1"
 export default class App extends React.Component {
-  
-  getLocation = async() => {
+  state = {
+    isLoading: true
+  }
+
+  getWeather = async (latitude, longitude) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    const { data } = await axios.get(url)
+    console.log(data);
+  }
+
+  getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
-      const location = await Location.getCurrentPositionAsync();
-      console.log(location);
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find your position", "Allow me to see where you are:(")
     }
   }
 
-  componentDidMount(){
-    this.getLocation()
+  componentDidMount() {
+    this.getLocation();
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Loading />
-        <StatusBar style="auto" />
-      </View>
+      this.state.isLoading ? <Loading /> : (
+        <View style={styles.container}>
+          <Text>WTF</Text>
+          <StatusBar style="auto" />
+        </View>
+      )
     );
   }
 }
