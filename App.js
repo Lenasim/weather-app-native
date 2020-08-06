9d5253bf1e43820ef44598e64f0007dd0c7c8701
LenaSim <radical.rin@gmail.com>
2020-08-06 15:12:49 +0200

    Get current location by expo api

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import * as Location from 'expo-location';
+import Loading from './Loading';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    } catch (error) {
+      Alert.alert("Can't find your position", "Allow me to see where you are:(")
+    }
+  }
+
+  componentDidMount(){
+    this.getLocation()
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Loading />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
 });
